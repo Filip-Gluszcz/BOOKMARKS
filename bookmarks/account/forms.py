6863984ec747bd708184882b02be46b2c2743b44
Filip-Gluszcz <80832642+Filip-Gluszcz@users.noreply.github.com>
2022-08-10 20:50:30 +0200

    Profile model added
    Profile and user edit, registration form fix, messages.

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Hasło', widget=forms.PasswordInput)
@@ -12,7 +13,19 @@ class UserRegistrationForm(forms.ModelForm):
         fields = ('username', 'first_name', 'email')
 
     def clean_password2(self):
-        cleaned_data = self.changed_data
+        cleaned_data = self.cleaned_data
         if cleaned_data['password'] != cleaned_data['password2']:
             raise forms.ValidationError('Hasła nie są identyczne')
         return cleaned_data['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
+        widgets = {'date_of_birth': forms.SelectDateWidget(empty_label=('Rok', 'Miesiąc', 'Dzień'),)}
+        labels = {'date_of_birth': 'Data urodzenia', 'photo': 'Zdjęcie'}
