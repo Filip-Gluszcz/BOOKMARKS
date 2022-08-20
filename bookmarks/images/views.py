00948e503ee12909cb6d29d2209adb7c60211630
Filip-Gluszcz <80832642+Filip-Gluszcz@users.noreply.github.com>
2022-08-20 11:45:06 +0200

    Images app
    Image model, create image using JavaScript bookmarklet, AJAX images list, image detail, AJAX image like/dislike

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from common.decorators import ajax_required
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from .forms import ImageCreateForm
+from .models import Image
 
-# Create your views here.
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+
+        if form.is_valid():
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Obraz został dodany')
+            return redirect(new_image.get_absolute_url())
+    
+    else: form = ImageCreateForm(data=request.GET)
+
+    return render(request, 'images/image/create.html', {'form': form, 'section': 'images'})
+
+@login_required
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like': image.users_like.add(request.user)
+            else: image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ok'})
+
+
+@login_required
+def image_list(request):
+    images_list = Image.objects.all()
+    paginator = Paginator(images_list, 8)
+    page = request.GET.get('page')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax(): return HttpResponse('')
+        images = paginator(paginator.num_pages)
+
+    if request.is_ajax(): return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
+
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
