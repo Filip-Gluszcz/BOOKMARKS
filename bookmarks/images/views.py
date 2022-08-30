117939be4d1e7f2a9350cd53e1e1e732629775a0
Filip-Gluszcz <80832642+Filip-Gluszcz@users.noreply.github.com>
2022-08-30 21:14:25 +0200

    Most viewed images
    Redis images ranking, image total likes (denormalization) updated by Django signal

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,12 +2,18 @@ from actions.utils import create_action
 from common.decorators import ajax_required
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from django.http import JsonResponse, HttpResponse
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
 from .models import Image
+import redis
+
+rds = redis.StrictRedis(host=settings.REDIS_HOST, 
+                        port=settings.REDIS_PORT, 
+                        db=settings.REDIS_DB)
 
 @login_required
 def image_create(request):
@@ -18,18 +24,45 @@ def image_create(request):
             new_image = form.save(commit=False)
             new_image.user = request.user
             new_image.save()
-            create_action(request.user, 'dodał obraz', new_image)
+            create_action(request.user, 
+                         'dodał obraz', 
+                          new_image)
             messages.success(request, 'Obraz został dodany')
             return redirect(new_image.get_absolute_url())
     
     else: form = ImageCreateForm(data=request.GET)
 
-    return render(request, 'images/image/create.html', {'form': form, 'section': 'images'})
+    return render(request, 
+                 'images/image/create.html', 
+                {'form': form, 
+                 'section': 'images'})
+
 
 @login_required
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    total_views = rds.incr(f'image:{image.id}:views')
+    rds.zincrby('image_ranking', 1, image.id)
+    return render(request, 
+                 'images/image/detail.html', 
+                {'section': 'images', 
+                 'image': image,
+                 'total_views': total_views})
+
+
+
+@login_required
+def image_ranking(request):
+    image_ranking = rds.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                  'most_viewed': most_viewed})
+
 
 @ajax_required
 @login_required
@@ -48,6 +81,7 @@ def image_like(request):
             return JsonResponse({'status': 'ok'})
         except:
             pass
+
     return JsonResponse({'status': 'ok'})
 
 
@@ -59,13 +93,19 @@ def image_list(request):
 
     try:
         images = paginator.page(page)
-    except PageNotAnInteger:
-        images = paginator.page(1)
+    except PageNotAnInteger: images = paginator.page(1)
     except EmptyPage:
-        if request.is_ajax():
-            return HttpResponse('')
+        if request.is_ajax(): return HttpResponse('')
         images = paginator(paginator.num_pages)
         
-    if request.is_ajax(): return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
+    if request.is_ajax(): return render(request, 
+                                       'images/image/list_ajax.html', 
+                                      {'section': 'images', 
+                                       'images': images})
 
-    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+    return render(request, 
+                 'images/image/list.html', 
+                {'section': 'images', 
+                 'images': images})
+
+
